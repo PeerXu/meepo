ad1a86654bda0fd25fa5e557042bd3b204512e99
PeerHui <pppeerxu@gmail.com>
2023-01-13 00:07:23 +0800

    bug: test failed

--- pkg/lib/rand/source.go
+++ pkg/lib/rand/source.go
@@ -1,5 +1,28 @@
 package rand
 
-import "math/rand"
+import (
+	"math/rand"
+	"sync"
+)
 
-var NewSource = rand.NewSource
+type lockedSource struct {
+	lk  sync.Mutex
+	src rand.Source
+}
+
+func (r *lockedSource) Int63() (n int64) {
+	r.lk.Lock()
+	n = r.src.Int63()
+	r.lk.Unlock()
+	return
+}
+
+func (r *lockedSource) Seed(seed int64) {
+	r.lk.Lock()
+	r.src.Seed(seed)
+	r.lk.Unlock()
+}
+
+func NewSource(seed int64) rand.Source {
+	return &lockedSource{src: rand.NewSource(seed)}
+}
